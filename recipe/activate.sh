b1dbbab9f8a4b90ff50d7f32371b2a105ea1d4c1
curtis.mayberry <curtisma3@gmail.com>
2022-08-15 19:53:57 -0700

    update recipe

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Default , if not defined by the user
-if [[ -z "${VIRTUE_SKILL_PREFIX}" ]]; then
-  # shellcheck disable=SC2016
-  echo 'Setting $VIRTUE_SKILL_PREFIX ='
-  echo "  ${CONDA_PREFIX}/lib/skill"
-  export VIRTUE_SKILL_PREFIX="${CONDA_PREFIX}/lib/skill"
-fi
-
-
 # Default cdsprj, can be overwritten by users
 if ! command -v cdsprj &> /dev/null
 then
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# Add SKILL library
-mkdir -p "${PREFIX}/lib/skill/virtue"
-cp -rf "virtue" \
-       "${PREFIX}/lib/skill/"
-# Not included in the sdist
-#for item in "README.md" "pyproject.toml"; do
-#    cp -rf "${RECIPE_DIR}/../${item}" \
-#           "${PREFIX}/lib/skill/virtue"
-#done
 
 # Copy the activate scripts to $PREFIX/etc/conda/activate.d.
 # This will allow them to be run on environment activation.
